952e2279e9332468d8f48a8ca444318cca157073
𝘗𝘰𝘮𝘦𝘭𝘦 <zjn0629@126.com>
2025-06-07 16:03:27 +0800

    native函数的示例

--- src/core/server/startup.ts
+++ src/core/server/startup.ts
@@ -4,16 +4,22 @@ import './secondFile.js'; // NOTE THE .js extension is required for file imports
 alt.log(`alt:V Server - Boilerplate Started`);
 alt.on('playerConnect', handlePlayerConnect);
 
-function handlePlayerConnect(player: alt.Player) {
+async function handlePlayerConnect(player: alt.Player) {
     alt.log(`[${player.id}] ${player.name} has connected to the server.`);
 
     player.model = 'mp_m_freemode_01';
     player.spawn(36.19486618041992, 859.3850708007812, 197.71343994140625, 0);
     
+    // 等待 1 秒
+    await alt.Utils.wait(1000)
+
     alt.emitClient(player, 'log:Console', 'alt:V Server - Boilerplate Started');
 
     // 向客户端发送一个名为 'do-something' 的事件，并传递参数 'hello'
     alt.emitClient(player, 'do-something', 'hello')
+
+    // 向客户端发送一个名为 'example' 的事件
+    alt.emitClient(player, 'example')
 }
 // 监听客户端发来的名为 'do-something' 的事件，并绑定处理函数 doSomething
 alt.onClient('do-something', doSomething);
